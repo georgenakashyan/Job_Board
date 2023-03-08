b0c6042ae1b8d9b778725f6fb7edbf5b38b92077
George-Macbook <nakagk@farmingdale.edu>
2023-03-07 21:48:15 -0500

    Created Abstract Classes: User and ModeratableUser. Created classes: Moderator, Poster, and User.

---
+++ src/main/java/com/group/job_board/ModeratableUser.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.group.job_board;
+
+/**
+ *
+ * @author main
+ */
+public class ModeratableUser extends User{
+    
+}
---
+++ src/main/java/com/group/job_board/User.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.group.job_board;
+
+/**
+ *
+ * @author George / Tim
+ */
+public abstract class User {
+    protected String username;
+}
