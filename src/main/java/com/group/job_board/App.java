12ba6fda9f534c002e30d4f3fbef81430f2aa8cc
Juilliard <Juilliardwu0405@gmail.com>
2023-03-08 01:28:53 -0500

    Disable window resize, user will not be able to change the window size.

--- src/main/java/com/group/job_board/App.java
+++ src/main/java/com/group/job_board/App.java
@@ -20,6 +20,7 @@ public class App extends Application {
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
+        stage.setResizable(false);
     }
 
     static void setRoot(String fxml) throws IOException {
