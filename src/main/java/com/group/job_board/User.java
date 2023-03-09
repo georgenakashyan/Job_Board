920dc0f11805d75b49be8347d0d75f8221650c67
smytts <smytts@farmingdale.edu>
2023-03-09 17:30:01 -0500

    Added more member variables (userID, firstName, lastName, password) and made constructors and setter/getter methods.

--- src/main/java/com/group/job_board/User.java
+++ src/main/java/com/group/job_board/User.java
@@ -9,5 +9,67 @@ package com.group.job_board;
  * @author George / Tim
  */
 public abstract class User {
-    protected String username;
+
+    //ToDo 1: make member variables
+    protected int userID;
+    protected String firstName, lastName, username, password;
+
+    //ToDo 2: make constructors
+    public User() {
+        this.userID = 0;
+        this.firstName = "";
+        this.lastName = "";
+        this.username = "";
+        this.password = "";
+    }
+
+    public User(int userID, String firstName, String lastName, String username, String password) {
+        this.userID = userID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    //ToDo 3: make getters and setters
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
