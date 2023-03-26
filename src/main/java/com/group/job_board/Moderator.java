31be779861a16f44c9f2a9136477b71276b19b1b
smytts <smytts@farmingdale.edu>
2023-03-26 10:27:59 -0400

    Added member variables and created method prototypes

--- src/main/java/com/group/job_board/Moderator.java
+++ src/main/java/com/group/job_board/Moderator.java
@@ -8,6 +8,29 @@ package com.group.job_board;
  *
  * @author main
  */
-public class Moderator extends User{
-    
+public class Moderator extends User {
+
+    public Moderator() {
+    }
+
+    public Moderator(int userID, String username, String password) {
+        super(userID, username, password);
+    }
+
+    // method prototypes
+    public void login() {
+    }
+
+    public void logout() {
+    }
+
+    private static void viewJobs() {
+    }
+
+    private static void approveJobs() {
+    }
+
+    public static void removeUser() {
+    }
+
 }
