7355369165a1dab48d7fe8b090fe562d32cd43f8
smytts <smytts@farmingdale.edu>
2023-03-26 10:27:54 -0400

    Added member variables and created method prototypes

--- src/main/java/com/group/job_board/Poster.java
+++ src/main/java/com/group/job_board/Poster.java
@@ -8,6 +8,53 @@ package com.group.job_board;
  *
  * @author main
  */
-public class Poster extends ModeratableUser{
+public class Poster extends ModeratableUser implements Jobs {
+
+    // Employer variables
+    String companyName;
+
+    // constructor
+    public Poster(String companyName) {
+        this.companyName = companyName;
+    }
     
+    // getters and setters
+    public Poster(int userID, String companyName, String username, String password) {
+        super(userID, username, password);
+        this.companyName = companyName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+    
+    // prototype methods
+
+    @Override
+    public Jobs addJob() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Jobs search() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void apply() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void viewJob() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+
+   
+
 }
