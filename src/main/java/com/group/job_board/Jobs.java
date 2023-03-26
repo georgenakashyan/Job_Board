ebddcaba1921abf984a346820c53ebbe12bc0406
smytts <smytts@farmingdale.edu>
2023-03-26 10:26:37 -0400

    Created interface and added method prototypes

---
+++ src/main/java/com/group/job_board/Jobs.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.group.job_board;
+
+/**
+ *
+ * @author main
+ */
+public interface Jobs {
+    // prototpye method
+
+    public Jobs addJob();
+
+    public Jobs search();
+
+    public void apply();
+
+    public void viewJob();
+
+}
