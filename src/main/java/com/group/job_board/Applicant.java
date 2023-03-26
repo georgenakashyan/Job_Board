fc171a4fa9f5d54f92d38c93a114bb8b6a8ac222
smytts <smytts@farmingdale.edu>
2023-03-26 10:27:47 -0400

    Added member variables and created method prototypes

--- src/main/java/com/group/job_board/Applicant.java
+++ src/main/java/com/group/job_board/Applicant.java
@@ -8,6 +8,72 @@ package com.group.job_board;
  *
  * @author main
  */
-public class Applicant extends ModeratableUser{
-    
+public class Applicant extends ModeratableUser implements Jobs {
+
+    protected String firstName, lastName;
+
+    public Applicant(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Applicant(int userID, String firstName, String lastName, String username, String password) {
+        super(userID, username, password);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    // prototype methods
+    public Resume createResume() {
+
+        return null;
+
+    }
+
+    public void updateProfile() {
+    }
+
+    public void deleteProfile() {
+    }
+
+    public Resume downloadResume() {
+        return null;
+    }
+
+    @Override
+    public Jobs addJob() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Jobs search() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void apply() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void viewJob() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
 }
