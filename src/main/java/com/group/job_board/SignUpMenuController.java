681a8a994442dc03213f1321c00c8cf12254fca1
Juilliard <Juilliardwu0405@gmail.com>
2023-04-03 17:05:04 -0400

    After success sign up switch to sign up menu

--- src/main/java/com/group/job_board/SignUpMenuController.java
+++ src/main/java/com/group/job_board/SignUpMenuController.java
@@ -8,6 +8,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -97,6 +99,11 @@ public class SignUpMenuController {
             
             //FOR TESTING PURPOSES ONLY
             System.out.println("Inserted successfully");
+        try {
+            switchToLogInMenu();
+        } catch (IOException ex) {
+            Logger.getLogger(SignUpMenuController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public void connectDB() throws ClassNotFoundException, SQLException { 
