ab9e4d9bc604f1e3ee4899e59f19ae471709cdda
smytts <smytts@farmingdale.edu>
2023-04-09 16:42:51 -0400

    Made sure the member variables for all classes matched the database tables and fixed all constructors and setters/getters as a result. Wrote method to load in applicants from database Overrode the toString method

--- src/main/java/com/group/job_board/Applicant.java
+++ src/main/java/com/group/job_board/Applicant.java
@@ -4,23 +4,26 @@
  */
 package com.group.job_board;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  *
  * @author main
  */
-public class Applicant extends ModeratableUser implements Jobs {
+public class Applicant extends ModeratableUser implements Position {
 
     protected String firstName, lastName;
 
-    public Applicant(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public Applicant() {
     }
 
-    public Applicant(int userID, String firstName, String lastName, String username, String password) {
-        super(userID, username, password);
+    public Applicant(int userID, String firstName, String lastName, String email, int phoneNumber, String address, String password) {
+        super(userID, email, phoneNumber, address, password);
         this.firstName = firstName;
         this.lastName = lastName;
+        this.status = false;
     }
 
     public String getFirstName() {
@@ -40,29 +43,64 @@ public class Applicant extends ModeratableUser implements Jobs {
     }
 
     // prototype methods
-    public Resume createResume() {
+    @Override
+    public void loadUsers() {
+        //create the variables for each field in the file
+        ArrayList<Applicant> applicants = new ArrayList();
+        int id = 0;
+        String firstname = "";
+        String lastname = "";
+        String email = "";
+        int phone = 0;
+        String address = "";
+        String password = "";
+        boolean active = false;
+        int totalrows = 0, index = 0;
 
-        return null;
+        connectDB();
 
+        try {
+            //Get the total rows in the table to loop through the result set
+            resultSet = statement.executeQuery("SELECT * FROM applicant");
+            //resultSet.first();
+
+            totalrows = resultSet.getRow();
+            while (resultSet.next()) //tests for the eof
+            {
+                totalrows = resultSet.getRow();
+                id = resultSet.getInt("ID");
+                firstname = resultSet.getString("firstName");
+                lastname = resultSet.getString("lastName");
+                email = resultSet.getString("email");
+                phone = resultSet.getInt("phone");
+                address = resultSet.getString("address");
+                password = resultSet.getString("password");
+                active = resultSet.getBoolean("active");
+                Applicant a = new Applicant();
+                applicants.add(a);
+                index++;
+
+            }
+            System.out.println(applicants.get(0).toString());
+        } catch (SQLException e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
     }
 
+    // ToDo write code for updateProfile(), deleteProfile(), search(), and apply()
     public void updateProfile() {
     }
 
     public void deleteProfile() {
     }
 
-    public Resume downloadResume() {
-        return null;
-    }
-
     @Override
-    public Jobs addJob() {
+    public Position addPosition() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public Jobs search() {
+    public Position search() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
@@ -72,8 +110,16 @@ public class Applicant extends ModeratableUser implements Jobs {
     }
 
     @Override
-    public void viewJob() {
+    public void viewPosition() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    @Override
+    public String toString() {
+        // int userID, String firstName, String lastName, String email, int phoneNumber, String address, String password
+        return "Applicant{" + "user ID: " + userID + ", first name: " + firstName
+                + ", last name:" + lastName + ", email: " + email + ", "
+                + ", phone number: " + phoneNumber + ", address: " + address
+                + ", password: " + password + '}';
+    }
 }
