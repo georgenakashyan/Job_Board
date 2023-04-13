1fe7d0f87d12a44a5be47122f2e2903e991aa940
Juilliard <Juilliardwu0405@gmail.com>
2023-04-13 14:42:22 -0400

    Created member variables and method structure for settings menu.

--- src/main/java/com/group/job_board/SettingsMenuController.java
+++ src/main/java/com/group/job_board/SettingsMenuController.java
@@ -1,4 +1,9 @@
 package com.group.job_board;
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -11,5 +16,55 @@ package com.group.job_board;
  * @author juilliardwu
  */
 public class SettingsMenuController {
+    @FXML
+    private TextField firstName;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private TextField address;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField phoneNumber;
+    @FXML
+    private TextField userName;
+    @FXML
+    private TextField accountType;
+    @FXML
+    private TextField oldPassword;
+    @FXML
+    private TextField newPassword;
+    @FXML
+    private Button ChangePasswordButton;
+    @FXML
+    private Button SignOutButton;
+    @FXML
+    private Button DeleteAccountButton;
     
+    @FXML
+    private void initialize() {
+        //Set all textfield variables to show user's current information.
+    }
+   
+    @FXML
+    private void SignOutButtonHandler() throws IOException{
+        // Sign them out of their account
+        // Send to loginMenu page
+        switchToLogInMenu();
+    }
+    
+    @FXML
+    private void DeleteAccountHandler(){
+        
+    }
+    
+    @FXML
+    private void changePassword() {
+        
+    }
+    
+    @FXML
+    private void switchToLogInMenu() throws IOException {
+        App.setRoot("LoginMenu");
+    }
 }
