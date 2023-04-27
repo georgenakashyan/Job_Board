273707e4acdf835d0446c5e1e56b6f2e1801922b
Juilliard <Juilliardwu0405@gmail.com>
2023-04-27 13:54:52 -0400

    Added new moderator menus

--- src/main/java/com/group/job_board/AddNewModMenuController.java
+++ src/main/java/com/group/job_board/AddNewModMenuController.java
@@ -1,14 +1,15 @@
+package com.group.job_board;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.group.job_board;
 
 /**
  *
  * @author juilliardwu
  */
-public class ModerationMenuController {
+public class AddNewModMenuController {
     
 }
