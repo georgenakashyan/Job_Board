18d6278c925f40051c90485e885519107cd42e34
George-Macbook <nakagk@farmingdale.edu>
2023-04-30 03:14:34 -0400

    Got rid of SQL code and unused imports.

--- src/main/java/com/group/job_board/SignUpMenuController.java
+++ src/main/java/com/group/job_board/SignUpMenuController.java
@@ -1,14 +1,9 @@
 package com.group.job_board;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
@@ -32,8 +27,6 @@ public class SignUpMenuController {
     @FXML
     private TextField phoneNumber;
     @FXML
-    private Label errorMessage;
-    @FXML
     private TextField userName;
     @FXML
     private TextField hidePassword;
@@ -43,21 +36,33 @@ public class SignUpMenuController {
     private TextField showReEnterPassword;
     @FXML
     private TextField hideReEnterPassword;
+    
     @FXML
     private ImageView eyeCloseIcon;
     @FXML
     private ImageView eyeOpenIcon;
-
-    private Boolean passwordShowing;
     @FXML
     private ChoiceBox UserIdentityChoiceBox;
-
-    Connection connection;
-    Statement statement;
-    ResultSet resultSet;
+    @FXML
+    private Label errorMessage;
     
-    ObservableList<String> UserIdentity = FXCollections.observableArrayList("Employer","Applicant");
+    private Boolean passwordShowing;
+    ObservableList<String> UserIdentity;
 
+    /**
+     * initialize set default as not showing password.
+     */
+    @FXML
+    private void initialize() {
+        showPassword.setVisible(false);
+        showReEnterPassword.setVisible(false);
+        eyeOpenIcon.setVisible(false);
+        errorMessage.setText("");
+        passwordShowing = false;
+        UserIdentityChoiceBox.setItems(UserIdentity);
+        
+    }
+    
     @FXML
     private void switchToLogInMenu() throws IOException {
         App.setRoot("LoginMenu");
@@ -114,33 +119,7 @@ public class SignUpMenuController {
         return "";
     }
 
-    public void connectDB() throws ClassNotFoundException, SQLException { 
-        Class.forName("net.ucanaccess.jdbc.UcanaccessDriver"); 		 
-        // Step 2: Opening database connection
-        String msAccDB = "codeangels.accdb";
-        String dbURL = "jdbc:ucanaccess://" + msAccDB; 
-        // Step 3: Create and get connection using DriverManager class
-        connection = DriverManager.getConnection(dbURL); 
-        // Step 4: Creating JDBC Statement 
-        // It is scrollable so we can use next() and last() & It is updatable so we can enter new records
-        statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
-        ResultSet.CONCUR_UPDATABLE);
-        System.out.println("Database Connected!");
-    }
-
-    /**
-     * initialize set default as not showing password.
-     */
-    @FXML
-    private void initialize() {
-        showPassword.setVisible(false);
-        showReEnterPassword.setVisible(false);
-        eyeOpenIcon.setVisible(false);
-        errorMessage.setText("");
-        passwordShowing = false;
-        UserIdentityChoiceBox.setItems(UserIdentity);
-        
-    }
+    
     
     /**
      * When user clicks either the open or closed eye icon
