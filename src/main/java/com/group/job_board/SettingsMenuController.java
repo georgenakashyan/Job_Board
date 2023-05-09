41623362af323110922f5e6d2d048baeb44422ad
George-Macbook <nakagk@farmingdale.edu>
2023-05-08 23:09:35 -0400

    Fixed Settings size

--- src/main/java/com/group/job_board/SettingsMenuController.java
+++ src/main/java/com/group/job_board/SettingsMenuController.java
@@ -1,4 +1,5 @@
 package com.group.job_board;
+
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -8,10 +9,11 @@ import javafx.scene.layout.HBox;
 
 /**
  * FXML Controller class for settings menu
- * 
+ *
  * @author juilliardwu
  */
 public class SettingsMenuController {
+
     @FXML
     private TextField firstName;
     @FXML
@@ -42,61 +44,66 @@ public class SettingsMenuController {
     private HBox addNewMod;
     @FXML
     private HBox moderation;
-    
+
     @FXML
     private Label deleteCheckLabel;
     private int deleteCheck;
-    
+
     /**
      * Method to initialize the scene
      */
     @FXML
     private void initialize() {
         deleteCheckLabel.setVisible(false);
-        String s = App.currentUser.getClass().toString().replace("class com.group.job_board.", "");
-        switch (s) {
-            case "Applicant":
-                postingNewJob.setDisable(true);
-                postingNewJob.setVisible(false);
-                addNewMod.setDisable(true);
-                addNewMod.setVisible(false);
-                moderation.setDisable(true);
-                moderation.setVisible(false);
-                break;
-            case "Employer":
-                addNewMod.setDisable(true);
-                addNewMod.setVisible(false);
-                moderation.setDisable(true);
-                moderation.setVisible(false);
-                break;
-
-            case "Moderator":
-                postingNewJob.setDisable(true);
-                postingNewJob.setVisible(false);
-                break;
+        if (App.currentUser instanceof Applicant) {
+            postingNewJob.setDisable(true);
+            postingNewJob.setVisible(false);
+            addNewMod.setDisable(true);
+            addNewMod.setVisible(false);
+            moderation.setDisable(true);
+            moderation.setVisible(false);
+        } else if (App.currentUser instanceof Employer) {
+            addNewMod.setDisable(true);
+            addNewMod.setVisible(false);
+            moderation.setDisable(true);
+            moderation.setVisible(false);
+        } else if (App.currentUser instanceof Moderator) {
+            postingNewJob.setDisable(true);
+            postingNewJob.setVisible(false);
         }
-        
+
         //Set all textfield variables to show user's current information.
     }
-   
+
+    @FXML
+    private void fillInInfo() {
+        if (App.currentUser instanceof Applicant) {
+            
+        } else if (App.currentUser instanceof Employer) {
+            
+        } else if (App.currentUser instanceof Moderator) {
+            
+        }
+    }
+
     /**
      * Method to log the user out of the program
-     * 
+     *
      * @throws IOException
      */
     @FXML
-    private void SignOutButtonHandler() throws IOException{
+    private void SignOutButtonHandler() throws IOException {
         FirestoreContext.logout();
         switchToLogInMenu();
     }
-    
+
     /**
      * Method to delete a user account
-     * 
+     *
      * @throws IOException
      */
     @FXML
-    private void DeleteAccountHandler() throws IOException{
+    private void DeleteAccountHandler() throws IOException {
         if (deleteCheck < 1) {
             deleteCheckLabel.setVisible(true);
             deleteCheck++;
@@ -106,29 +113,29 @@ public class SettingsMenuController {
             switchToLogInMenu();
         }
     }
-    
+
     /**
      * Method to change user password
-     * 
+     *
      */
     @FXML
     private void changePassword() {
-        
+
     }
-    
+
     /**
      * Method to switch to login menu scene
-     * 
+     *
      * @throws IOException
      */
     @FXML
     private void switchToLogInMenu() throws IOException {
         App.setRoot("LoginMenu");
     }
-    
+
     /**
      * Method to switch to job posting menu scene
-     * 
+     *
      * @throws IOException
      */
     @FXML
@@ -138,37 +145,37 @@ public class SettingsMenuController {
 
     /**
      * Method to switch to settings scene
-     * 
+     *
      * @throws IOException
      */
     @FXML
     private void switchToSettingsMenu() throws IOException {
         App.setRoot("SettingsMenu");
     }
-    
+
     /**
      * Method to switch to new job posting menu scene
-     * 
+     *
      * @throws IOException
      */
     @FXML
     private void switchToPostingNewJobMenu() throws IOException {
         App.setRoot("PostingNewJobMenu");
     }
-    
+
     /**
      * Method to switch to add new moderator menu scene
-     * 
+     *
      * @throws IOException
      */
     @FXML
     private void switchToAddNewModMenu() throws IOException {
         App.setRoot("AddNewModMenu");
     }
-    
+
     /**
      * Method to switch to moderator menu scene
-     * 
+     *
      * @throws IOException
      */
     @FXML
