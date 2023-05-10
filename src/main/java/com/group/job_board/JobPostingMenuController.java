c9055542b05e8b1b1b42d7db21524018449ee0ea
George-Macbook <nakagk@farmingdale.edu>
2023-05-09 22:36:23 -0400

    Dropdown menus

--- src/main/java/com/group/job_board/JobPostingMenuController.java
+++ src/main/java/com/group/job_board/JobPostingMenuController.java
@@ -2,8 +2,10 @@ package com.group.job_board;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -38,7 +40,19 @@ public class JobPostingMenuController {
     private Label paid;
     @FXML
     private Label description;
-    
+    @FXML
+    private ChoiceBox stateFilter;
+    private ObservableList<String> stateItems;
+    @FXML
+    private ChoiceBox placeFilter;
+    private ObservableList<String> placeItems;
+    @FXML
+    private ChoiceBox typeFilter;
+    private ObservableList<String> typeItems;
+    @FXML
+    private ChoiceBox payFilter;
+    private ObservableList<String> payItems;
+
     /**
      * FXML fields for this controller
      */
@@ -52,6 +66,23 @@ public class JobPostingMenuController {
      */
     @FXML
     private void initialize() {
+        stateItems = FXCollections.observableArrayList("AL", "AK", "AZ", "AR", "CA",
+                "CO", "CT", "DE", "FL", "GA",
+                "HI", "ID", "IL", "IN", "IA",
+                "KS", "KY", "LA", "ME", "MD",
+                "MA", "MI", "MN", "MS", "MO",
+                "MT", "NE", "NV", "NH", "NJ",
+                "NM", "NY", "NC", "ND", "OH",
+                "OK", "OR", "PA", "RI", "SC",
+                "SD", "TN", "TX", "UT", "VT",
+                "VA", "WA", "WV", "WI", "WY");
+        stateFilter.setItems(stateItems);
+        placeItems = FXCollections.observableArrayList("On-site", "Hybrid", "Remote");
+        placeFilter.setItems(placeItems);
+        typeItems = FXCollections.observableArrayList("Full-Time", "Part-Time", "Intern");
+        typeFilter.setItems(typeItems);
+        payItems = FXCollections.observableArrayList("$15-$20", "$20-$25", "$25-$30", "$30-$40", "$40+");
+        payFilter.setItems(payItems);
         description.setWrapText(true);
         items = listviewJobs.getItems();
         jobArr = new ArrayList<>();
@@ -78,7 +109,59 @@ public class JobPostingMenuController {
         }
         loadJobs();
     }
-    
+
+    @FXML
+    private void filterState() {
+        int index = stateFilter.getSelectionModel().getSelectedIndex();
+        if (index != -1) {
+            String filter = stateItems.get(index);
+            for (Position p : jobArr) {
+                if (!p.getState().equals(filter)) {
+                    jobArr.remove(p);
+                }
+            }
+        }
+    }
+
+    @FXML
+    private void filterPlace() {
+        int index = placeFilter.getSelectionModel().getSelectedIndex();
+        if (index != -1) {
+            String filter = placeItems.get(index);
+            for (Position p : jobArr) {
+                if (!p.getType().equals(filter)) {
+                    jobArr.remove(p);
+                }
+            }
+        }
+    }
+
+    @FXML
+    private void filterType() {
+        int index = typeFilter.getSelectionModel().getSelectedIndex();
+        if (index != -1) {
+            String filter = typeItems.get(index);
+            for (Position p : jobArr) {
+                if (!p.getRemote().equals(filter)) {
+                    jobArr.remove(p);
+                }
+            }
+        }
+    }
+
+    @FXML
+    private void filterPay() {
+        int index = payFilter.getSelectionModel().getSelectedIndex();
+        if (index != -1) {
+            String filter = payItems.get(index);
+            for (Position p : jobArr) {
+                if (String.valueOf(p.getPay()).contains(filter.substring(1, 2))) {
+                    jobArr.remove(p);
+                }
+            }
+        }
+    }
+
     /**
      * Method to search the job postings
      */
@@ -92,7 +175,7 @@ public class JobPostingMenuController {
         items.clear();
         String s = App.currentUser.getClass().toString().replace("class com.group.job_board.", "");
         if (s.contains("Employer")) {
-            Employer emp = (Employer)App.currentUser;
+            Employer emp = (Employer) App.currentUser;
             jobArr = FirestoreContext.searchEmployerJobPostings(emp.getCompanyName(), searchBar.getText());
         } else {
             jobArr = FirestoreContext.searchJobPostings(searchBar.getText());
@@ -101,7 +184,7 @@ public class JobPostingMenuController {
             items.add(String.format("Company - %s\n   Job - %s\n   $%.2f", p.getCompany(), p.getTitle(), p.getPay()));
         }
     }
-    
+
     /**
      * Method to load jobs from the database
      */
@@ -110,7 +193,7 @@ public class JobPostingMenuController {
         items.clear();
         String s = App.currentUser.getClass().toString().replace("class com.group.job_board.", "");
         if (s.equals("Employer")) {
-            Employer emp = (Employer)App.currentUser;
+            Employer emp = (Employer) App.currentUser;
             jobArr = FirestoreContext.getEmployerJobPostings(emp.getCompanyName());
             return;
         } else {
@@ -120,7 +203,7 @@ public class JobPostingMenuController {
             items.add(String.format("Company - %s\n   Job - %s\n   $%.2f", p.getCompany(), p.getTitle(), p.getPay()));
         }
     }
-    
+
     @FXML
     private void handleJobClick() {
         int selection = listviewJobs.getSelectionModel().getSelectedIndex();
@@ -139,6 +222,7 @@ public class JobPostingMenuController {
 
     /**
      * Method to switch to job posting menu
+     *
      * @throws IOException
      */
     @FXML
@@ -148,33 +232,37 @@ public class JobPostingMenuController {
 
     /**
      * Method to switch to the settings menu
+     *
      * @throws IOException
      */
     @FXML
     private void switchToSettingsMenu() throws IOException {
         App.setRoot("SettingsMenu");
     }
-    
+
     /**
      * Method to switch to the posting new job menu
+     *
      * @throws IOException
      */
     @FXML
     private void switchToPostingNewJobMenu() throws IOException {
         App.setRoot("PostingNewJobMenu");
     }
-    
+
     /**
      * Method to switch to the add new moderator menu
+     *
      * @throws IOException
      */
     @FXML
     private void switchToAddNewModMenu() throws IOException {
         App.setRoot("AddNewModMenu");
     }
-    
+
     /**
      * Method to switch to moderator menu
+     *
      * @throws IOException
      */
     @FXML
